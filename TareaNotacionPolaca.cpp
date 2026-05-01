60cb3bde4c672f84c0fdc2829ac7b75875edb84e
Luciana Milagros Farfan Begazo <lucianafarfanbegazo@gmail.com>
2026-04-30 21:00:08 -0500

    Super codigo super resuelto por super por fin

--- TareaNotacionPolaca.cpp
+++ TareaNotacionPolaca.cpp
@@ -1,28 +1,60 @@
 #include <iostream>
 
+int Precedencia(char val) {
+    if (val == '+' || val == '-') { return 1; }
+    else if (val == '*' || val == '/') { return 2; }
+    return -1;
+}
+
+bool OperadorPreguntaSuperPreguntosa(char val) {
+    return((val == '+') || (val == '-') || (val == '*') || (val == '/') || (val == '(') || (val == ')'));
+}
+
+int Opera(char car, int a, int b) {
+    if (car == '+') {
+        return (a + b);
+    }
+
+    else if (car == '-') {
+        return (a - b);
+    }
+
+    else if (car == '*') {
+        return a * b;
+    }
+
+    else if (car == '/') {
+        return a /b ;
+    }
+}
+
+template <class T>
 struct Node {
-    char data;
+    T data;
     Node* next;
     Node* prev;
 
-    Node(char val) {
+    Node(T val) {
         data = val;
         next = 0;
         prev = 0;
     }
 };
 
+template <class T>
 struct LinkedList {
-    Node* tail;
-    Node* head;
+    Node<T>* tail;
+    Node<T>* head;
+    int num_elem;
 
     LinkedList() {
         tail = 0;
         head = 0;
+        num_elem = 0;
     }
 
     ~LinkedList() {
-        Node* temp = head;
+        Node<T>* temp = head;
         while (head) {
             head = temp->next;
             delete temp;
@@ -33,8 +65,8 @@ struct LinkedList {
         head = 0;
     }
 
-    void push_front(char val) {
-        Node* t = new Node(val);
+    void push_front(T val) {
+        Node<T>* t = new Node(val);
 
         if (!head) {
             head = t;
@@ -46,10 +78,12 @@ struct LinkedList {
             head->prev = t;
             head = t;
         }
+
+        num_elem++;
     }
 
-    void push_back(char val) {
-        Node* t = new Node(val);
+    void push_back(T val) {
+        Node<T>* t = new Node(val);
         if (!head) {
             head = t;
             tail = t;
@@ -60,10 +94,11 @@ struct LinkedList {
             tail->next = t;
             tail = t;
         }
+        num_elem++;
         return;
     }
 
-    void pop_front(char &val) {
+    void pop_front(T &val) {
         val = head->data;
         if (head == tail) {
             delete head;
@@ -76,10 +111,12 @@ struct LinkedList {
             delete head->prev;
             head->prev = 0;
         }
+
+        num_elem--;
         return;
     }
 
-    void pop_back(char &val) {
+    void pop_back(T &val) {
         val = tail->data;
         if (head == tail) {
             delete tail;
@@ -92,11 +129,12 @@ struct LinkedList {
             delete tail->next;
             tail->next = 0;
         }
+        num_elem--;
         return;
     }
 
     void print() {
-        Node* temp = tail;
+        Node<T>* temp = tail;
         if (!temp) { return; }
         while (temp) {
             std::cout << temp->data << " ";
@@ -105,42 +143,36 @@ struct LinkedList {
 
         std::cout << std::endl;
     }
+
+    
 };
-
-int Precedencia(char val) {
-    if (val == '+' || val == '-') { return 1; }
-    else if (val == '*' || val == '/') { return 2;}
-    return -1;
-}
-
-bool OperadorPreguntaSuperPreguntosaPorqueSiYYaMeQuieroDormirPorfaFunciona(char val) {
-    return((val == '+') || (val == '-') || (val == '*') || (val == '/') || (val == '(') || (val == ')'));
-}
 
 int main()
 {
-    char Operacion[30] = "(A+(B*C-(D/E*F)*G)*H)";
-    LinkedList Stack;
-    LinkedList Result;
-    char a;
+    char Operacion[12] = "3+4*2/(1-5)";
+    LinkedList<char> Stack;
+    LinkedList<char> Result;
+    LinkedList<int> Solve;
+    char basurachar;
+    int basuraint;
 
     char* p = Operacion;
     while (*p != '\0') {
-        if (!OperadorPreguntaSuperPreguntosaPorqueSiYYaMeQuieroDormirPorfaFunciona(*p)) { Result.push_front(*p);}
+        if (!OperadorPreguntaSuperPreguntosa(*p)) { Result.push_front(*p);}
         else if (*p == '(') { Stack.push_front(*p); }
 
         else if (*p == ')') {
             while (Stack.head->data != '(') {
                 Result.push_front(Stack.head->data);
-                Stack.pop_front(a);
+                Stack.pop_front(basurachar);
             }
-            Stack.pop_front(a);
+            Stack.pop_front(basurachar);
         }
 
         else {
             if ((Stack.head)&&(Precedencia(*p) <= Precedencia((Stack.head->data)))) {
                 Result.push_front(Stack.head->data);
-                Stack.pop_front(a);
+                Stack.pop_front(basurachar);
                 Stack.push_front(*p);
             }
             else { Stack.push_front(*p); }
@@ -156,11 +188,33 @@ int main()
 
     while (Stack.head) {
         Result.push_front(Stack.head->data);
-        Stack.pop_front(a);
+        Stack.pop_front(basurachar);
     }
 
+    std::cout << "Super Resultado: ";
     Result.print();
+    std::cout << std::endl;
+    
+    if (Result.num_elem > 2) {
+        while (Result.tail) {
 
+            if (OperadorPreguntaSuperPreguntosa(Result.tail->data)) {
+                int b = Solve.tail->data;
+                int a = Solve.tail->prev->data;
+                Solve.pop_back(basuraint);
+                Solve.pop_back(basuraint);
+
+                Solve.push_back(Opera(Result.tail->data,a,b));
+            } 
+            else {
+                Solve.push_back(Result.tail->data - 48);
+            }
+            Result.pop_back(basurachar);
+        }
+    }
+
+    std::cout << "Super Resultado Super Resuelto: ";
+    Solve.print();
 
 }
 
